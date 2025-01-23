df2c00599726ec9402539187d420ce869b28c82d
hmsou <hmarcs66@gmail.com>
2025-01-23 17:45:52 -0300

    Add Question Controller

---
+++ src/main/java/com/hmso/cadastro/controller/QuestionController.java
@@ -0,0 +1,42 @@
+package com.hmso.cadastro.controller;
+
+import com.hmso.cadastro.dominio.Question;
+import com.hmso.cadastro.repository.QuestionRepository;
+import com.hmso.cadastro.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("questions")
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    public Question addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
+    @GetMapping
+    public List<Question> findAllQuestions(){
+        return questionService.findAllQuestions();
+    }
+
+    @PutMapping(path = "/{question_id}")
+    public Question updateQuestion(
+            @PathVariable Long question_id,
+            @RequestBody Question question){
+        return questionService.updateQuestion(question_id, question);
+    }
+
+    @DeleteMapping(path = "/{question_id}")
+    public void deleteQuestion(@PathVariable Long question_id){
+        questionService.deleteQuestion(question_id);
+    }
+}
