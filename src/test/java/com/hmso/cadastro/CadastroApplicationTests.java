3cc7bd3d59551a68b5914ef235c33eb0445d9532
hmsou <hmarcs66@gmail.com>
2025-01-22 23:37:19 -0300

    Initial Commit

---
+++ src/test/java/com/hmso/cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmso.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
