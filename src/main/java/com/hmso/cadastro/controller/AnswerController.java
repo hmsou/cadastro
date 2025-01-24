cb54c9213c9dfd3ab8e528d27dfad656a5b95864
hmsou <hmarcs66@gmail.com>
2025-01-24 19:08:30 -0300

    Add Http Status for Answer

--- src/main/java/com/hmso/cadastro/controller/AnswerController.java
+++ src/main/java/com/hmso/cadastro/controller/AnswerController.java
@@ -5,6 +5,8 @@ import com.hmso.cadastro.dominio.entities.Answer;
 import com.hmso.cadastro.mappers.AnswerMapper;
 import com.hmso.cadastro.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,35 +24,36 @@ public class AnswerController {
 
 
     @PostMapping(path= "/{person_id}/{question_id}")
-    public Answer addAnswer(@PathVariable Long person_id,
-                            @PathVariable Long question_id,
-                            @RequestBody Answer answer){
-        return answerService.addAnswer(person_id, question_id, answer);
+    public ResponseEntity<Answer> addAnswer(@PathVariable Long person_id,
+                                           @PathVariable Long question_id,
+                                           @RequestBody Answer answer){
+        return new ResponseEntity<>(answerService.addAnswer(person_id, question_id, answer), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<AnswerDto> findAnswers(){
+    public ResponseEntity<List<AnswerDto>> findAnswers(){
 
-        return answerService.findAnswers()
+        return new ResponseEntity<>(answerService.findAnswers()
                 .stream()
                 .map(answerMapper::toDto)
-                .toList();
+                .toList(), HttpStatus.OK);
     }
 
     @GetMapping(path = "{answer_id}")
-    public Answer findAnswerById(@PathVariable Long answer_id){
-        return answerService.findAnswerById(answer_id);
+    public ResponseEntity<Answer> findAnswerById(@PathVariable Long answer_id){
+        return new ResponseEntity<>(answerService.findAnswerById(answer_id), HttpStatus.OK);
     }
 
     @PutMapping(path = "/{answer_id}")
-    public Answer updateAnswer(
+    public ResponseEntity<Answer> updateAnswer(
             @PathVariable Long answer_id,
             @RequestBody Answer answer){
-        return answerService.updateAnswer(answer_id, answer);
+        return new ResponseEntity<>(answerService.updateAnswer(answer_id, answer), HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{answer_id}")
-    public void deleteAnswer(@PathVariable Long answer_id){
+    public ResponseEntity<Void> deleteAnswer(@PathVariable Long answer_id){
         answerService.deleteAnswer(answer_id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
